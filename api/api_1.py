7a436029099fd830bbd6595f5ef38261ed0cf4cd
ravindaa <ravindaa@oregonstate.edu>
2023-12-17 15:33:13 +0200

    Updated API and HTML

--- api/api_1.py
+++ api/api_1.py
@@ -15,5 +15,5 @@ app.add_middleware(
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
-    return {"item_id": item_id, "name": "Item Name", "description": "A description of the item"}
+    return {"item_id": item_id, "name": "First Item", "description": "This is the first time."}
 
