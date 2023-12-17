dfacaeca09f25574645ec32e6d53cef8ab8a8238
ravindaa <ravindaa@oregonstate.edu>
2023-12-17 15:19:30 +0200

    Updated API code

--- api/api_1.py
+++ api/api_1.py
@@ -1,10 +1,19 @@
-# Import FastAPI
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-# Create an instance of the FastAPI class
 app = FastAPI()
 
-# Define a route
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, World!"}
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id, "name": "Item Name", "description": "A description of the item"}
+
